ea49115c4159d5507d6bc02f4021105e9a05a4b2
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-14 02:35:49 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_Quatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                soma = soma + i;
+            }
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
